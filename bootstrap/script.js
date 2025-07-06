3ebc66fcc18afbb0d12925991bb33098886a45c1
Augusto <augustoneto0109@gmail.com>
2025-07-06 12:44:56 -0300

    Delete bootstrap directory

--- bootstrap/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const botao = document.getElementById('btnMensagem');
-  const divMensagem = document.getElementById('mensagem');
-  botao.addEventListener('click', () => {
-    console.log('Cada passo te leva mais perto do seu objetivo.');
-    divMensagem.textContent = 'Cada passo te leva mais perto do seu objetivo!';});});
